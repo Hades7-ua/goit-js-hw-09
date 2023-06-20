bb90459b7012542f1b263d19a373bee72cd08510
Hades7-ua <114616094+Hades7-ua@users.noreply.github.com>
2023-06-20 18:46:41 +0000

    Deploying to gh-pages from @ Hades7-ua/goit-js-hw-09@b8b4bb4c162cf05ed857e4d69d9b0482c1252f4f 🚀

---
+++ 01-color-switcher.75cabd4e.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");let d=null;t.addEventListener("click",(function(){t.setAttribute("disabled",""),e.removeAttribute("disabled"),d=setInterval((()=>{!function(){const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;r.style.backgroundColor=t}()}),1e3)})),e.addEventListener("click",(function(){clearInterval(d),t.removeAttribute("disabled"),e.setAttribute("disabled","")})),e.setAttribute("disabled","");
+//# sourceMappingURL=01-color-switcher.75cabd4e.js.map
